d620e0c027578ba917d9bfd42325e6d7f96d8d86
Jorrit Tinholt <dejorrit@gmail.com>
2018-04-09 17:42:36 +0200

    Updated demo

--- demo/src/demo.js
+++ demo/src/demo.js
@@ -2,5 +2,7 @@ import Zinch from '../../dist/zinch';
 
 let images = document.querySelectorAll('.js-image');
 images.forEach(image => {
-	new Zinch(image, {});
+	new Zinch(image, {
+		duration: 350
+	});
 });
