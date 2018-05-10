2f8686623f2c480f1acb0555c6977e9185eee83c
Jorrit Tinholt <dejorrit@gmail.com>
2018-05-10 15:44:34 +0200

    Improved build

--- webpack.config.js
+++ webpack.config.js
@@ -1,32 +1,43 @@
 const path = require('path');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 
 const config = {
-	entry: './src/js/index.js',
+	entry: {
+		'cinemaZoom': './src/js/index.js',
+		'cinemaZoom.min': './src/js/index.js',
+	},
 	output: {
 		path: path.resolve(__dirname, 'dist'),
-		filename: 'cinemaZoom.js',
+		filename: '[name].js',
 		libraryTarget: 'umd',
 	},
 	module: {
-		rules: [
-			{
-				test: /\.js$/,
-				use: [{
-					loader: "babel-loader",
-				}]
-			},
-			{
-				test: /\.scss$/,
-				use: [{
-					loader: "style-loader"
-				}, {
-					loader: "css-loader"
-				}, {
-					loader: "sass-loader"
-				}]
+		rules: [{
+			test: /\.js$/,
+			use: [{
+				loader: "babel-loader",
+			}]
+		}, {
+			test: /\.scss$/,
+			use: [{
+				loader: "style-loader"
+			}, {
+				loader: "css-loader"
+			}, {
+				loader: "sass-loader"
+			}]
+		}]
+	},
+	plugins: [
+		new UglifyJsPlugin({
+			test: /\.min.js$/,
+			cache: true,
+			parallel: true,
+			uglifyOptions: {
+				toplevel: true,
 			}
-		]
-	}
+		})
+	]
 };
 
 module.exports = config;
